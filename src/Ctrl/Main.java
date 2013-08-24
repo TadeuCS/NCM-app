a13471511b3c6e0c4de0df499f161e0b6e14d0da
Tadeu <Tadeu@Tadeu-PC>
2013-08-24 09:52:31 -0300

    7º commit

---
+++ src/Ctrl/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ctrl;
+
+import View.Principal;
+
+/**
+ *
+ * @author Tadeu
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal p= new Principal();
+        p.setVisible(true);
+    }
+}
